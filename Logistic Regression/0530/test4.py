e38fd55213e77051b585f1bff3d6f8a7c6b2dab0
jammmin02 <jmkm1201@naver.com>
2025-06-13 16:10:54 +0900

    0530

--- Logistic Regression/0530/test4.py
+++ Logistic Regression/0530/test4.py
@@ -4,5 +4,9 @@ one_hot = np.eye(4)
 
 print(one_hot, "\n\n")
 
-my_list = [1, 2]
-print(one_hot[my_list])
+my_list = [0, 1, 0, 3, 2, 3]
+
+one_hot_list = one_hot[my_list]
+
+print(one_hot_list)
+print(np.argmax(one_hot_list, axis=1))
